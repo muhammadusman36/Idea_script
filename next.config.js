f484a0a5900aad52e72bd897fae34dbcc48ab74e
unknown <saummanzoor49@gmail.com>
2023-12-11 17:43:56 +0500

    project-completed

--- next.config.js
+++ next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+    images:{
+        remotePatterns:[{
+            hostname:"files.edgestore.dev"
+        }]
+        // domains:["files.edgestore.dev"]
+    }
+}
 
 module.exports = nextConfig
